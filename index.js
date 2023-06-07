08909d1b5e1044618988f3de916490dd5855cdd5
John Dunn <john.dunn@qsc.com>
2023-06-07 16:19:12 -0600

    send more info

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ wss.on('connection', function connection(ws, req) {
 
   ws.on('message', function message(data, isBinary) {
     console.log('received: %s', data);
-    ws.send(`got message ${data}, isBinary ${isBinary}`);
+    let rsp = {};
+    rsp.state = "message"
+    rsp.data = data;
+    rsp.isBinary = isBinary;
+    ws.send(JSON.stringify(rsp));
     
   });
 
   let data = {};
+  data.state = "connected"
   data.path = req.url;
   data.protocol = ws.protocol;
   data.subprotocol = ws.subprotocol;
